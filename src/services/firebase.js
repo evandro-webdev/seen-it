dfafc940534cc66b5396dd2bcfc82fd203ebdb4e
evandro-webdev <evandromateus06@gmail.com>
2026-06-19 16:17:20 -0300

    wip

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,23 +1,26 @@
 import { initializeApp } from "firebase/app";
 
-import { 
-  getFirestore, 
-  collection, 
-  doc,  
-  addDoc, 
-  getDocs, 
-  deleteDoc 
+import {
+  getFirestore,
+  collection,
+  doc,
+  addDoc,
+  getDocs,
+  deleteDoc,
 } from "firebase/firestore";
 
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "my-movie-circle.firebaseapp.com",
+  authDomain:
+    window.location.hostname === "localhost"
+      ? "my-movie-circle.firebaseapp.com"
+      : window.location.hostname,
   projectId: "my-movie-circle",
   storageBucket: "my-movie-circle.appspot.com",
   messagingSenderId: "509365004119",
-  appId: "1:509365004119:web:cac637bcf57aaaef1c4012"
+  appId: "1:509365004119:web:cac637bcf57aaaef1c4012",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -25,12 +28,4 @@ const db = getFirestore(app);
 
 const auth = getAuth(app);
 
-export { 
-  db, 
-  collection, 
-  doc, 
-  addDoc, 
-  getDocs, 
-  deleteDoc,
-  auth
-};
+export { db, collection, doc, addDoc, getDocs, deleteDoc, auth };
