74fc4b66ce853d53166912e06bb13b45dc8ee45b
evandro-webdev <evandromateus06@gmail.com>
2026-09-23 13:08:20 -0300

    feat: create BaseSelect generic component

---
+++ src/components/forms/BaseSelect.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+  options: {
+    type: Array,
+    required: true,
+  },
+});
+
+const modelValue = defineModel({ type: String, default: "" });
+</script>
+
+<template>
+  <select
+    class="bg-transparent font-medium border-none focus:outline-none focus:ring-0 cursor-pointer p-0 text-xs text-ellipsis overflow-hidden whitespace-nowrap max-w-[105px] sm:max-w-none"
+  >
+    <option
+      v-for="option in options"
+      :key="option.value"
+      :value="option.value"
+      class="dark:bg-[#121825]"
+    >
+      {{ option.label }}
+    </option>
+  </select>
+</template>
